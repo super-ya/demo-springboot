5eb6928ffc2a98e0845410b871f7269ba403a115
杨栋 <353982557@qq.com>
2020-06-02 11:25:52 +0800

    测试提交本地

--- src/main/java/com/example/demo/test/Thing.java
+++ src/main/java/com/example/demo/test/Thing.java
@@ -10,7 +10,7 @@ import lombok.Data;
  **/
 class Demo {
     public static void main(String[] args) throws CloneNotSupportedException {
-        User user = new User("四哥");
+        User user = new User("四哥666");
         Thing thing = new Thing("ab", "hello", user);
 
         Object clone = thing.clone();
